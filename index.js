e1430da909e815ea1cc6494efbe60e9037a9f146
Mayasoft Ingenieria <infraestructura@mayasoft.com.co>
2023-04-17 15:12:50 -0500

    subiendo cambios de jenkins

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use('/persona', personaRoute);
 
 app.get('/', (req, res) => {
-  res.send("hola mundos");
+  res.send("Hola soy AI, no te sorprendas");
 });
 
 app.listen(port, () => {
